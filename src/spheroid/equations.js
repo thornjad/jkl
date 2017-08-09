7a5704c9b51c8a670ef0edf4f3f29de4aea182c2
Jade Thornton <Jade.Thornton@thomsonreuters.com>
2017-08-09 16:56:47 -0500

    break 3d equations into smaller chunks

--- src/spheroid/equations.js
+++ src/spheroid/equations.js
@@ -6,44 +6,63 @@ exports.maxDepth = maxDepth;
 exports.earthRadius = earthRadius;
 
 function distanceBetween3D(point1, point2) {
-  if(point1.x === undefined || point1.y === undefined || point1.z === undefined) {
-    console.log("Invalid point 1: Must define x, y, and z coordinates");
-  }
-
-  if(point2.x === undefined || point2.y === undefined || point2.z === undefined) {
-    console.log("Invalid point 2: Must define x, y, and z coordinates");
-  }
-
-  if(typeof point1.x === "number" && typeof point1.y === "number" &&
-     typeof point1.z === "number" && typeof point2.x === "number" &&
-     typeof point2.y === "number" && typeof point2.z === "number") {
+  if (pointsAreValid(point1, point2)) {
     return Math.sqrt( Math.pow((point1.x - point2.x), 2) +
                       Math.pow((point1.y - point2.y), 2) +
                       Math.pow((point1.z - point2.z), 2) );
   }
 }
 
+const pointsAreValid = (point1, point2) =>
+  pointsAreDefined(point1, point2) &&
+  pointsAreNum(point1, point2);
+
+const pointsAreDefined = (point1, point2) => {
+  if (pointIsDefined(point1)) {
+    console.log('Invalid point 1: Must define x, y, and z coordinates');
+    return false;
+  }
+
+  if (pointIsDefined(point2)) {
+    console.log('Invalid point 2: Must define x, y, and z coordinates');
+    return false;
+  }
+
+  return true;
+};
+
+const pointIsDefined = point => point1.x === undefined || point1.y === undefined || point1.z === undefined;
+
+const pointsAreNum = (point1, point2) =>
+  typeof point1.x === 'number' &&
+  typeof point1.y === 'number' &&
+  typeof point1.z === 'number' &&
+  typeof point2.x === 'number' &&
+  typeof point2.y === 'number' &&
+  typeof point2.z === 'number';
+
 function latLongToPoint(lat, long, radius=earthRadius) {
-  let phi = (90 - lat) * (Math.PI / 180);
-  let theta = (long + 180) * (Math.PI / 180);
+  const phi = (90 - lat) * (Math.PI / 180);
+  const theta = (long + 180) * (Math.PI / 180);
 
   x = -((radius) * Math.sin(phi) * Math.cos(theta));
   y = ((radius) * Math.cos(phi));
   z = ((radius) * Math.sin(phi) * Math.sin(theta));
 
-  let point = {x: x, y: y, z: z};
+  const point = {x: x, y: y, z: z};
   return point;
 }
 
 function distanceBetween2D(point1, point2) {
-  console.log("Not defined because I'm lazy");
+  console.log('THE EARTH IS AN OBLIQUE SPHEROID');
 }
 
-// Because of trigonometry, we know that the maximum "depth" of a chord across
+// Because of trigonometry, we know that the maximum 'depth' of a chord across
 // a circle is equal to the radius minus the square root of the radius squared
-// minus one quarter of the distance between the two points squared
+// minus one quarter of the distance between the two points squared. In other
+// words, because math.
 function maxDepth(point1, point2, radius=earthRadius) {
-  let dist = distanceBetween3D(point1, point2);
-  let depth = radius - Math.sqrt(Math.pow(radius, 2) - (0.25 * Math.pow(dist, 2)));
+  const dist = distanceBetween3D(point1, point2);
+  const depth = radius - Math.sqrt(Math.pow(radius, 2) - (0.25 * Math.pow(dist, 2)));
   return depth;
 }
