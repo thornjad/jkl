bd5dea2f7f1f56fa5e8a6e9bc2cdd71a85e8b6b8
Jade Thornton <Jade.Thornton@thomsonreuters.com>
2017-08-10 09:17:44 -0500

    rename to jkl

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ const program = require('commander');
 const co = require('co');
 const prompt = require('co-prompt');
 
-const main = require('./spheroid/main');
-
-const version = '0.1.0';
+const main = require('./jkl/main');
+const pjson = require('../package.json')
 
 let testtmp;
 
 program
-  .version(version)
+  .version(pjson.version)
   .arguments('[tst]')
   .option('-b, --bbq', 'Add BBQ sauce')
+  .option('-f, --flat', 'Assume Earth is flat')
   .action(function(tst) {
     console.log('hello');
   })
   .parse(process.argv);
 
 co(function * () {
-  console.log(`Spheroid ${version}`);
+  console.log(`jkl ${pjson.version}`);
 })
